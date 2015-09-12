2ee187e33fafff777243bd6199adb49bcfc56284
nikhil-kulkarni <nikhil896@gmail.com>
2015-09-11 22:45:43 -0400

    Location manager added

--- Trayl/ViewController.swift
+++ Trayl/ViewController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var postItemButton: UIButton!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.postItemButton.layer.shadowColor = UIColor.grayColor().CGColor
+        self.postItemButton.layer.shadowOffset = CGSizeMake(0, 1.0)
+        self.postItemButton.layer.shadowOpacity = 1.0
+        self.postItemButton.layer.shadowRadius = 8.0
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let titleDict: NSDictionary = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        self.navigationController!.navigationBar.titleTextAttributes = titleDict as! [String : AnyObject]
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.AuthorizedAlways {
+            locationManager.startUpdatingLocation()
+        } else {
+            // TODO: If user does not authorize
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        print(location?.coordinate)
     }
 
     override func didReceiveMemoryWarning() {
